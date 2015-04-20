5141272dba530157e7739c280f0cad04b1fcff32
Colin Torney <colin.j.torney@gmail.com>
2015-04-20 13:42:01 +0100

    full rotation invariance

--- extractor/cudaHOGExtractor.py
+++ extractor/cudaHOGExtractor.py
@@ -1,6 +1,4 @@
-from SimpleCV.base import *
-from SimpleCV.ImageClass import Image
-from SimpleCV.Features.FeatureExtractorBase import *
+
 import pycuda.elementwise as elementwise
 import pycuda.autoinit
 import pycuda.gpuarray as gpuarray
@@ -9,10 +7,11 @@ import scikits.cuda.linalg as culinalg
 import scikits.cuda.misc as cumisc
 culinalg.init()
 import cmath as cm
+import numpy as np
 
 from pycuda.compiler import SourceModule
 
-class cudaHOGExtractor(FeatureExtractorBase):
+class cudaHOGExtractor():
     """
     Create a 1D edge length histogram and 1D edge angle histogram.
     
@@ -30,23 +29,14 @@ class cudaHOGExtractor(FeatureExtractorBase):
         # number of fourier modes that will be used (0:modes-1)
         self.mNMaxFreq = max_freq 
 
-        mf = self.mNMaxFreq
-        self.mNCount = ((mf+1)*(mf+1) + mf+1 + mf*(mf+1))*(bins-1) + (mf+1)
-
+        mf = self.mNMaxFreq+1
+        self.mNCount = 2*(bins-1) * (mf + 2*(np.dot([mf - i for i in range(mf)] , range(mf))  ))
+        self.mNBinCount = 2* (mf + 2*(np.dot([mf - i for i in range(mf)] , range(mf))  ))
         # create a list to store kernels for regional descriptors based on circular harmonics
         self.ciKernel = []
         self.gpu_histF=None
 
-        # first create the central region 
-        [x,y]=np.meshgrid(range(-self.mNSize+1,self.mNSize),range(-self.mNSize+1,self.mNSize))
-        z = x + 1j*y
-        kernel = self.mNSize - np.abs(z)
-        kernel[kernel < 0] = 0
-        kernel = kernel/sum(sum(kernel))
-
-        self.ciKernel.append(kernel)
-
-        # next build the internal regions - (bins-1) concentric circles
+        # build the internal regions - (bins-1) concentric circles
         modes = range(0, self.mNMaxFreq+1)
         scale = range(2, self.mNBins+1)
 
@@ -86,7 +76,7 @@ class cudaHOGExtractor(FeatureExtractorBase):
         phi = np.angle(dz)
         r = np.abs(dz)
  #       r = r/(r.mean()+0.001)
-	
+
 
         # create an empty array for storing the dfft of the orientation vector
         histF = np.zeros([nx, ny, self.mNMaxFreq+1])+0j
@@ -100,18 +90,19 @@ class cudaHOGExtractor(FeatureExtractorBase):
 
         # compute regional descriptors by convolutions (these descriptors are not rotation invariant)
         fHOG = np.zeros([self.mNCount])
+        fHOG2 = np.zeros([self.mNCount])
         f_index = 0
         template = self.ciKernel[0]
         (tnx, tny) = template.shape
         tnx2 = int(round(0.5*tnx))
         featureDetail = []
         for k in range(0,self.mNMaxFreq+1):
-            fHOG[f_index] = abs(np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template))))
+            fHOG[f_index] = 0.0#abs(np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template))))
             f_index+=1
         #   featureDetail.append([0,-1,-k, 0, -k])
 
 
-        scale = range(2, self.mNBins+1)
+        scale = range(0, self.mNBins-1)
         #for s in scale:
          #   featureDim = 0;
           #  for freq in range(-self.mNMaxFreq,self.mNMaxFreq+1):
@@ -124,29 +115,59 @@ class cudaHOGExtractor(FeatureExtractorBase):
         # no descriptors should be conjugate as this gives no extra information, 
         # i.e. we want to keep (UmFk OR U-mF-k) and (U-mFk OR UmF-k). If m=k then 
         # this is a rotation invariant feature, if not we only keep the magnitude
+#        for s in scale:
+#            for freq in range(0,self.mNMaxFreq+1):
+#                k_index = 0 + (s-0)*(self.mNMaxFreq+1)+abs(freq)
+#print k_index
+#                template = self.ciKernel[k_index]
+#                (tnx, tny) = template.shape
+#                tnx2 = int(round(0.5*tnx))
+#                for k in range(0,self.mNMaxFreq+1):
+#                    if (k==freq):
+#                        fHOG[f_index] = np.real(np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template))))
+#                        f_index+=1
+#                        fHOG[f_index] = np.imag(np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template))))
+#                        f_index+=1
+#                    else:
+#                        fHOG[f_index] = abs(np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template))))
+#                        f_index+=1#
+
+#                    if(k > 0):
+#                        template = np.conjugate(template)
+#                        fHOG[f_index] = abs(np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template))))
+#                        f_index+=1
+
+
+        tebhw = 0
+        f_index = 0
         for s in scale:
+            allVals = np.zeros((self.mNMaxFreq+1,self.mNMaxFreq+1),dtype=np.complex64)
             for freq in range(0,self.mNMaxFreq+1):
-                k_index = 1 + (s-2)*(self.mNMaxFreq+1)+abs(freq)
-                template = self.ciKernel[k_index]
+                template = self.ciKernel[s*(self.mNMaxFreq+1)+freq]
                 (tnx, tny) = template.shape
                 tnx2 = int(round(0.5*tnx))
                 for k in range(0,self.mNMaxFreq+1):
-                    if (k==freq):
-                        fHOG[f_index] = np.real(np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template))))
-                        f_index+=1
-                        fHOG[f_index] = np.imag(np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template))))
-                        f_index+=1
-                    else:
-                        fHOG[f_index] = abs(np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template))))
-                        f_index+=1
+                    allVals[freq,k] = np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template)))
+            for (x,y), val in np.ndenumerate(allVals):
+                if x==y:
+                    fHOG2[f_index]=val.real
+                    f_index+=1
+                    fHOG2[f_index]=val.imag
+                    f_index+=1
+                    tebhw+=1
+                else:
+                    for (x1,y1), val1 in np.ndenumerate(allVals):
+                        if x1<x: continue
+                        if y1<y: continue
+                        if (x-y)==(x1-y1):
+                            fHOG2[f_index]=(val*val1.conjugate()).real
+                            f_index+=1
+                            fHOG2[f_index]=(val*val1.conjugate()).imag
+                            f_index+=1
+                            tebhw+=1
+#        print tebhw, f_index
 
-                    if(k > 0):
-                        template = np.conjugate(template)
-                        fHOG[f_index] = abs(np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template))))
-                        f_index+=1
-
-
-        return fHOG.tolist()
+        return fHOG2.tolist()
 
     def prepareExtract(self, img):
 
@@ -159,7 +180,7 @@ class cudaHOGExtractor(FeatureExtractorBase):
 
         bx = nx//maxThreads
         by = ny//maxThreads
-        features = np.zeros((nx,ny,self.mNCount),dtype=np.float32)
+        #features = np.zeros((nx,ny,self.mNCount),dtype=np.float32)
 
         
         cx = int(round(0.5*nx))
@@ -185,6 +206,17 @@ class cudaHOGExtractor(FeatureExtractorBase):
 
         func(dz_gpu, self.gpu_histF, np.int32(self.mNMaxFreq+1), np.int32(nx), np.int32(ny), block = (maxThreads, maxThreads, 1), grid = (bx, by, 1))
 
+
+        # create an empty array for storing the dfft of the orientation vector
+        histF = np.zeros([self.mNMaxFreq+1,nx, ny ])+0j
+
+        # take the dfft of the orientation vector up to order MaxFreq
+        # positive values of k only since negative values give conjugate
+        for k in range(0,self.mNMaxFreq+1):
+            histF[k,:,:] = np.multiply(np.exp( -1j * (k) * phi) , r+0j)
+
+        allVals = self.gpu_histF.get()
+        print np.max(abs(allVals-histF))
 #       hf_out = np.empty_like(histF)
 #       hf_out = gpu_histF.get()#np.empty_like(a)
 
@@ -212,75 +244,31 @@ class cudaHOGExtractor(FeatureExtractorBase):
 
         gpu_positions = gpuarray.to_gpu(positions.astype(np.int32))
         features = np.zeros((N,self.mNCount),dtype=np.float32)
-        func2 = self.mod.get_function("d_convolve")
-
+        func2 = self.mod.get_function("d_convolve2")
+        func3 = self.mod.get_function("d_rotation_inv")
         # compute regional descriptors by convolutions (these descriptors are not rotation invariant)
         fHOG = np.zeros([self.mNCount])
         f_index = 0
         template = self.ciKernel[0].astype(np.complex64)
         gpu_template = gpuarray.to_gpu(template)
-        feature_slice = np.zeros((N),dtype=np.float32)
+        feature_slice = np.zeros((N,self.mNCount),dtype=np.float32)
         gpu_features = gpuarray.to_gpu(feature_slice)
-        (tnx, tny) = template.shape
-        tnx2 = int(round(0.5*tnx))
-        for k in range(0,self.mNMaxFreq+1):
-#           fHOG[f_index] = abs(np.sum(np.sum(np.multiply(histF[cx-tnx2:cx-tnx2+tnx,cy-tnx2:cy-tnx2+tnx,k],template))))
-            func2(gpu_features, self.gpu_histF, gpu_positions, gpu_template, np.int32(N), np.int32(k), np.int32(tnx), np.int32(nx), np.int32(ny), np.int32(f_index), np.int32(0), block = (maxThreads, 1, 1), grid = (bx, 1, 1))
-            feature_slice = gpu_features.get()#np.empty_like(a)
-            features[:,f_index]=feature_slice
-            f_index+=1
-#print 'done ' + str(f_index)  + ' of ' + str(self.mNCount)
-        #   featureDetail.append([0,-1,-k, 0, -k])
 
-
-
-        scale = range(2, self.mNBins+1)
-        #for s in scale:
-         #   featureDim = 0;
-          #  for freq in range(-self.mNMaxFreq,self.mNMaxFreq+1):
-           #     for k in range(0,self.mNMaxFreq+1):
-            #        ff = freq - k
-             #       if(ff >= -self.mNMaxFreq and ff <= self.mNMaxFreq and not(k==0 and freq < 0)):
-              #          featureDim = featureDim + 1;
-               #         featureDetail.append([s,-1,-k, freq, ff])
-
-        # no descriptors should be conjugate as this gives no extra information, 
-        # i.e. we want to keep (UmFk OR U-mF-k) and (U-mFk OR UmF-k). If m=k then 
-        # this is a rotation invariant feature, if not we only keep the magnitude
+        scale = range(0, self.mNBins-1)
         for s in scale:
+            allVals = np.zeros((N,self.mNMaxFreq+1,self.mNMaxFreq+1),dtype=np.complex64)
+            gpu_vals = gpuarray.to_gpu(allVals)
             for freq in range(0,self.mNMaxFreq+1):
-                k_index = 1 + (s-2)*(self.mNMaxFreq+1)+abs(freq)
-                template = self.ciKernel[k_index].astype(np.complex64)
+                template = self.ciKernel[s*(self.mNMaxFreq+1)+freq].astype(np.complex64)
                 gpu_template = gpuarray.to_gpu(template)
                 (tnx, tny) = template.shape
+                print tnx
                 tnx2 = int(round(0.5*tnx))
-                for k in range(0,self.mNMaxFreq+1):
-                    if (k==freq):
-#           func2(gpu_features, self.gpu_histF, gpu_template, np.int32(k), np.int32(tnx), np.int32(nx), np.int32(ny), np.int32(f_index), np.int32(1), block = (maxThreads, maxThreads, 1), grid = (bx, by, 1))
-                        func2(gpu_features, self.gpu_histF, gpu_positions, gpu_template, np.int32(N), np.int32(k), np.int32(tnx), np.int32(nx), np.int32(ny), np.int32(f_index), np.int32(1), block = (maxThreads, 1, 1), grid = (bx, 1, 1))
-                        feature_slice = gpu_features.get()#np.empty_like(a)
-                        features[:,f_index]=feature_slice
-                        f_index+=1
-#    func2(gpu_features, self.gpu_histF, gpu_template, np.int32(k), np.int32(tnx), np.int32(nx), np.int32(ny), np.int32(f_index), np.int32(2), block = (maxThreads, maxThreads, 1), grid = (bx, by, 1))
-                        func2(gpu_features, self.gpu_histF, gpu_positions, gpu_template, np.int32(N), np.int32(k), np.int32(tnx), np.int32(nx), np.int32(ny), np.int32(f_index), np.int32(2), block = (maxThreads, 1, 1), grid = (bx, 1, 1))
-                        feature_slice = gpu_features.get()#np.empty_like(a)
-                        features[:,f_index]=feature_slice
-                        f_index+=1
-                    else:
-#                       func2(gpu_features, self.gpu_histF, gpu_template, np.int32(k), np.int32(tnx), np.int32(nx), np.int32(ny), np.int32(f_index), np.int32(0), block = (maxThreads, maxThreads, 1), grid = (bx, by, 1))
-                        func2(gpu_features, self.gpu_histF, gpu_positions, gpu_template, np.int32(N), np.int32(k), np.int32(tnx), np.int32(nx), np.int32(ny), np.int32(f_index), np.int32(0), block = (maxThreads, 1, 1), grid = (bx, 1, 1))
-                        feature_slice = gpu_features.get()#np.empty_like(a)
-                        features[:,f_index]=feature_slice
-                        f_index+=1
-
-                    if(k > 0):
-                        template = np.conjugate(template).astype(np.complex64)
-                        gpu_template = gpuarray.to_gpu(template)
-#func2(gpu_features, self.gpu_histF, gpu_template, np.int32(k), np.int32(tnx), np.int32(nx), np.int32(ny), np.int32(f_index), np.int32(0), block = (maxThreads, maxThreads, 1), grid = (bx, by, 1))
-                        func2(gpu_features, self.gpu_histF, gpu_positions, gpu_template, np.int32(N), np.int32(k), np.int32(tnx), np.int32(nx), np.int32(ny), np.int32(f_index), np.int32(0), block = (maxThreads, 1, 1), grid = (bx, 1, 1))
-                        feature_slice = gpu_features.get()#np.empty_like(a)
-                        features[:,f_index]=feature_slice
-                        f_index+=1
+                func2(gpu_vals, self.gpu_histF, gpu_positions, gpu_template, np.int32(N), np.int32(self.mNMaxFreq+1), np.int32(tnx), np.int32(nx), np.int32(ny), np.int32(freq), np.int32(1), block = (maxThreads, 1, 1), grid = (bx, 1, 1))
+            allVals = gpu_vals.get()#np.empty_like(a)
+            func3(gpu_vals, gpu_features, np.int32(N), np.int32(self.mNMaxFreq+1), np.int32(s), block = (maxThreads, 1, 1), grid = (bx, 1, 1))
+            
+            break
 #      print 'done ' + str(f_index)  + ' of ' + str(self.mNCount)
         return features
 
